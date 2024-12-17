fd6275dad391c16f6322947f9429be4f3013f89e
Harini Mudaliar <harinimudaliar1503@gmail.com>
2024-12-17 20:14:29 +0530

    Adding some new changes

---
+++ route/listings.js
@@ -0,0 +1,81 @@
+const express= require("express");
+const router= express.Router({mergeParams: true});
+const {Listingschema}= require("../Schema");
+const ExpressError= require("../utils/ExpressError");
+const Listing = require("../models/listing");
+
+const validateListing= (err, req, res, next)=>{
+    let {error}= Listingschema.validate(req.body);
+        if(error){
+            let errmsg= error.details.map((el)=>el.message).join(",");
+            throw new ExpressError(400, errmsg);
+        }else{
+            next();
+        }
+}
+
+router.get("/", async(req,res)=>{
+    let allListings= await Listing.find({});
+    res.render("listings/indexroot.ejs", {allListings});
+})
+
+router.get("/listings", async(req,res)=>{
+    let allListings= await Listing.find({});
+    res.render("listings/indexroot.ejs", {allListings});
+})
+
+router.get("/listings/new", (req, res)=>{
+    res.render("listings/new.ejs");
+})
+
+router.post("/",validateListing,async(req, res, next)=>{
+        try{
+            const newListing= new Listing(req.body.listing);
+            await Listing.insertMany(newListing);
+            res.redirect("/listings");
+        }catch(err){
+            next(err);
+        }
+    })
+
+router.get("/:id", async(req,res,next)=>{
+    try{
+        let {id}= req.params;
+        const listing = await Listing.findById(id).populate({path: 'reviews'});  // Mongoose will handle the conversion internally
+        res.render('listings/show', { listing });
+    }catch(err){
+        next(err);
+    }
+})
+
+router.get("/:id/edit", async(req,res,next)=>{
+   try{
+    let{id}= req.params;
+    const listing= await Listing.findById(id);  // Mongoose will handle the conversion internally
+     res.render("listings/edit.ejs",{listing});
+   }catch(err){
+    next(err);
+   }
+});
+
+router.put("/:id",validateListing, async(req,res,next)=>{
+    try{
+        let {id}= req.params;
+        await Listing.findByIdAndUpdate(id, {...req.body.listing});;  // Mongoose will handle the conversion internally
+        res.redirect(`/listings/${id}`);
+    }catch(err){
+        next(err);
+    }
+})
+
+router.delete("/:id", async(req,res,next)=>{
+   try{
+    let {id}= req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+   }catch(err){
+    next(err);
+   }
+});
+
+module.exports= router;
---
+++ route/reviews.js
@@ -0,0 +1,47 @@
+const express= require("express");
+const router= express.Router({mergeParams: true});
+const {ReviewSchema}= require("../Schema");
+const ExpressError= require("../utils/ExpressError");
+const Listing = require("../models/listing");
+const Review = require("../models/review");
+
+const validateReview= (err, req, res, next)=>{
+    let {error}= ReviewSchema.validate(req.body);
+        if(error){
+            let errmsg= error.details.map((el)=>el.message).join(",");
+            throw new ExpressError(400, errmsg);
+        }else{
+            next();
+        }
+}
+
+//review
+//post review route
+router.post("/",validateReview, async(req,res,next)=>{
+    try{
+        let listing= await Listing.findById(req.params.id);
+        let newreview = new Review(req.body.review);
+
+        listing.reviews.push(newreview);
+        await newreview.save();
+        await listing.save();
+    
+        res.redirect(`/listings/${req.params.id}`)
+    }catch(err){
+        next(err);
+    }
+});
+ 
+//delete route for review
+router.delete("/:reviewId", async(req,res,next)=>{
+    try{
+        let {id,reviewId}= req.params;
+        await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+        await Review.findByIdAndDelete(reviewId);
+        res.redirect(`/listings/${id}`)
+    }catch(err){
+        next(err);
+    }
+})
+
+module.exports= router;
